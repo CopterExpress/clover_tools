d6a061cfe39ca50f1da4e279504d3704fa0e4079
Arthur Golubtsov <goldartt@gmail.com>
2019-08-27 13:41:01 +0100

    Small refactor of flight routines

--- clever_tools/src/flight_routines.py
+++ clever_tools/src/flight_routines.py
@@ -5,14 +5,13 @@ import time
 import logging
 import threading
 import rospy
+import copy
 from clever import srv
 from mavros_msgs.srv import SetMode
 from mavros_msgs.srv import CommandBool
 from std_srvs.srv import Trigger
 
-logger = logging.getLogger(__name__)
-
-# create proxies to services
+# Create proxies to services
 navigate = rospy.ServiceProxy('/navigate', srv.Navigate)
 set_position = rospy.ServiceProxy('/set_position', srv.SetPosition)
 set_rates = rospy.ServiceProxy('/set_rates', srv.SetRates)
@@ -21,9 +20,9 @@ get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 arming = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
 landing = rospy.ServiceProxy('/land', Trigger)
 
-logger.info("Proxy services inited")
+rospy.loginfo("Proxy services inited")
 
-# globals
+# Globals
 FREQUENCY = 40              # hz
 TOLERANCE = 0.2             # m
 SPEED = 1.0                 # m/s
@@ -42,28 +41,25 @@ def get_distance(x1, y1, z1, x2, y2, z2):
 
 # Takeoff to specified height
 def takeoff(height=TAKEOFF_HEIGHT, speed=TAKEOFF_SPEED, tolerance=TOLERANCE, frame_id=LOCAL_FRAME_ID):
-    logger.info("Takeoff started...")
+    rospy.loginfo("Takeoff started...")
     rate = rospy.Rate(FREQUENCY)
     start = get_telemetry(frame_id=frame_id)
+    climb = 0.
     result = navigate(z=height, speed=speed, yaw=float('nan'), frame_id=COPTER_FRAME_ID, auto_arm=True)
-    logger.info(result)
-    while True:
-        telem = get_telemetry(frame_id=frame_id)
-        climb = abs(telem.z - start.z)
-        logger.info("Takeoff to {:.2f} of {:.2f} meters".format(climb, height))
-        if abs(climb-height) < tolerance:
-            break
+    rospy.loginfo(result)
+    while abs(climb - height) > tolerance:
+        climb = abs(get_telemetry(frame_id=frame_id).z - start.z)
+        rospy.loginfo("Takeoff to {:.2f} of {:.2f} meters".format(climb, height))
         rate.sleep()
-    logger.info("Takeoff succeeded!")
+    rospy.loginfo("Takeoff succeeded!")
 
 # Reach specified point. Copter needs to be armed.
 def reach_point(x, y, z, yaw=float('nan'), speed=SPEED, tolerance=TOLERANCE, frame_id=LOCAL_FRAME_ID):
-    logger.info("Reaching point...")
+    rospy.loginfo("Reaching point...")
     rate = rospy.Rate(FREQUENCY)
+    telem = get_telemetry(frame_id=frame_id)
     result = navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, tolerance=tolerance, frame_id=frame_id)
-    logger.info(result)
-    while True:
+    rospy.loginfo(result)
+    while get_distance(x, y, z, telem.x, telem.y, telem.z) > tolerance:
         telem = get_telemetry(frame_id=frame_id)
-        if get_distance(x, y, z, telem.x, telem.y, telem.z) < tolerance:
-            break
         rate.sleep()
