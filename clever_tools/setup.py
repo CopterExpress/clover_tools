02cfe7d965f94898f10a158fac85c80da4a9e81a
Arthur Golubtsov <goldartt@gmail.com>
2019-09-02 10:50:55 +0100

    Add setup file to install flight_routines as python module

--- clever_tools/setup.py
+++ clever_tools/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
  
 setup_args = generate_distutils_setup(
-    packages=['clever_tools'],
+    packages=['flight_routines'],
     package_dir={'':'src'},
 )
  
